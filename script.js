a249b35206d184e451de1c9d343bf584e4b4185f
Shashank Soni <shashanksoni815@gmail.com>
2024-09-11 23:52:24 +0530

    process

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let a = 5;
+// let a = 5;
 
-for (let i = 0; i < 5; i++) {
-    console.log("Hello ", i);   
-}
+// for (let i = 0; i < 5; i++) {
+//     console.log("Hello ", i);   
+// }
 
-console.log("Bye");
+// console.log("Bye");
+
+console.log(process.argv);
