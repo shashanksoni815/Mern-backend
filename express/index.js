a484523f9e2b18f7c09ec6cf8a85fe1d78d9ea59
Shashank Soni <shashanksoni815@gmail.com>
2024-09-16 20:51:40 +0530

    server

--- express/index.js
+++ express/index.js
@@ -9,6 +9,24 @@ app.listen(port, () => {
     console.log(`app is listening onn port ${port}`);
 });
   
-app.use((req, res) => {
-    console.log("request recived");
+// app.use((req, res) => {
+//     // console.log(req);
+//     console.log("request recived");
+//     res.send("heyy... It's Shashank Soni");
+// });
+
+app.get("/", (req, res) => {
+    res.send("you contacted root path");
+});
+
+app.get("/apple", (req, res) => {
+    res.send("you contacted apple path");
+});
+
+app.get("/orange", (req, res) => {
+    res.send("you contacted orange path");
+});
+
+app.get("*", (req, res) => {
+    res.send("you contacted default path");
 });
