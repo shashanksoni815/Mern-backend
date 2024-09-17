1509d5099a629a8c6cdc171b52d4903dbea6caac
Shashank Soni <shashanksoni815@gmail.com>
2024-09-17 21:48:19 +0530

    EJS

--- EjsDir/index.js
+++ EjsDir/index.js
@@ -1,1 +1,10 @@
-ÿþ
+const express = require("express");
+const app = express();
+
+const port = 8080;
+
+app.get("view engine", "ejs");
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
