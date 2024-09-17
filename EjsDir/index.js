4e4fb7aadd8c694ee2e366390b9e66bfc0666c13
Shashank Soni <shashanksoni815@gmail.com>
2024-09-18 01:02:00 +0530

    json

--- EjsDir/index.js
+++ EjsDir/index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = 8080;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/set", (req, res) => {
-    res.render("home.ejs");
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("rollDice.ejs", {num: diceVal});
+});
+
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    const instaData = require("./views/data.json");
+    // console.log(instaData);
+    const data = instaData[username];
+    console.log(data);
+    res.render("instagram.ejs", {data});
 });
 
 app.listen(port, () => {
