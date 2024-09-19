2168ecfe7b0894a8b0d250b895c9408255dc2a42
Shashank Soni <shashanksoni815@gmail.com>
2024-09-19 21:52:11 +0530

    oops

---
+++ miscellaneous/frontent/app.js
@@ -0,0 +1,26 @@
+const stu1 = {
+    name: "aman",
+    age: 25,
+    marks: 95,
+    getMarks: function() {
+        return this.marks
+    },
+};
+
+const stu2 = {
+    name: "naman",
+    age: 25,
+    marks: 92,
+    getMarks: function() {
+        return this.marks
+    },
+};
+
+const stu3 = {
+    name: "shas",
+    age: 25,
+    marks: 85,
+    getMarks: function() {
+        return this.marks
+    },
+};
