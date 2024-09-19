c79c0530a717effd31e9c3cb60677ed7c580b8e0
Shashank Soni <shashanksoni815@gmail.com>
2024-09-19 21:40:43 +0530

    js

--- miscellaneous/backend/index.js
+++ miscellaneous/backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 app.get("/register", (req, res) => {
     let {user, password} = req.query;
     res.send(`standard GET responce. Welcome ${user} `);
