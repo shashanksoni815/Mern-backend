dbf89a9298da98f6f7b2d0f24bbc2b2a20a2baae
Shashank Soni <shashanksoni815@gmail.com>
2024-09-21 22:36:23 +0530

    arr

--- miscellaneous/frontent/app.js
+++ miscellaneous/frontent/app.js
@@ -1,26 +1,31 @@
-const stu1 = {
-    name: "aman",
-    age: 25,
-    marks: 95,
-    getMarks: function() {
-        return this.marks
-    },
-};
+// const stu1 = {
+//     name: "aman",
+//     age: 25,
+//     marks: 95,
+//     getMarks: function() {
+//         return this.marks
+//     },
+// };
 
-const stu2 = {
-    name: "naman",
-    age: 25,
-    marks: 92,
-    getMarks: function() {
-        return this.marks
-    },
-};
+// const stu2 = {
+//     name: "naman",
+//     age: 25,
+//     marks: 92,
+//     getMarks: function() {
+//         return this.marks
+//     },
+// };
 
-const stu3 = {
-    name: "shas",
-    age: 25,
-    marks: 85,
-    getMarks: function() {
-        return this.marks
-    },
+// const stu3 = {
+//     name: "shas",
+//     age: 25,
+//     marks: 85,
+//     getMarks: function() {
+//         return this.marks
+//     },
+// };
+
+let arr = [1, 2, 3];
+arr.sayHello = () => {
+    console.log("hello!, i am arr");
 };
