cf05d3a4f4bfac62928a35e45a5e1133fdb3c08b
Shashank Soni <shashanksoni815@gmail.com>
2024-09-24 22:58:02 +0530

    Fatory function

--- miscellaneous/frontent/app.js
+++ miscellaneous/frontent/app.js
@@ -34,3 +34,20 @@ arr.sayHello = () => {
 arr2.sayHello = () => {
     console.log("hello!, i am arr");
 };
+
+// factory function
+
+function PersonMaker(name, age) {
+    const person = {
+        name: name,
+        age: age,
+        talk() {
+            console.log(`Hi, my name is ${this.name}`);
+        },
+    };
+
+    return person;
+}
+
+let p1 = PersonMaker("adam", 25);
+let p2 = PersonMaker("eve", 25);
