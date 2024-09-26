839471a38458ab571772b07184474875a4310cdb
Shashank Soni <shashanksoni815@gmail.com>
2024-09-26 22:17:20 +0530

    ejs

--- Rest-Class/index.js
+++ Rest-Class/index.js
@@ -54,8 +54,8 @@ app.post("/posts", (req, res) => {
 app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
     let post = posts.find((p) => id === p.id);
-    console.log(post);
-    res.send("request working :)");
+    // console.log(post);
+    res.render("show.ejs", { post });
 });
 
 app.listen(port, () => {
