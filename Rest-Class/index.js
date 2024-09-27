207f846407fca380d9af347c85899f76e944fd83
Shashank Soni <shashanksoni815@gmail.com>
2024-09-27 23:26:19 +0530

    32

--- Rest-Class/index.js
+++ Rest-Class/index.js
@@ -46,7 +46,7 @@ app.get("/posts/new", (req, res) => {
 });
 
 app.post("/posts", (req, res) => {
-    // console.log(req.body);
+
     let { username, content } = req.body;
     let id = uuidv4();
     posts.push({id, username, content });
