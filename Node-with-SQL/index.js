baa0e7565e614e40d7bb1538b984b555a9cec3b8
Shashank Soni <shashanksoni815@gmail.com>
2024-10-01 20:00:13 +0530

    err

--- Node-with-SQL/index.js
+++ Node-with-SQL/index.js
@@ -2,18 +2,28 @@ const { faker } = require('@faker-js/faker');
 import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'test',
+  host: 'localhost',
+  user: 'root',
+  database: 'start',
+  password: '0832EC221034',
+});
+
+try {
+  connection.query("SHOW TABLES", (err, result) => {
+    if (err) throw err;
+    console.log(result);
   });
+} catch (err) {
+  console.log(err);
+}
 
 let getRandomUser = () => {
-    return {
-      id: faker.string.uuid(),
-      username: faker.internet.userName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
+  return {
+    id: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
 };
 
-console.log(getRandomUser());
+// console.log(getRandomUser());
