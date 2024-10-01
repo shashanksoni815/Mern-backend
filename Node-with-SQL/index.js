18af0f0816f733903687c8acdca1f5b02159c107
Shashank Soni <shashanksoni815@gmail.com>
2024-10-01 13:28:11 +0530

    sql

---
+++ Node-with-SQL/index.js
@@ -0,0 +1,12 @@
+const { faker } = require('@faker-js/faker');
+
+let getRandomUser = () => {
+    return {
+      id: faker.string.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+};
+
+console.log(getRandomUser());
