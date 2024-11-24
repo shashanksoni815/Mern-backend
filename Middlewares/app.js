6cd8e9ebe52d3750274a1d0646ffd32f020faa7a
Shashank Soni <shashanksoni815@gmail.com>
2024-11-24 18:07:59 +0530

    middleware

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
 
+// app.use((req, res, next) => {
+//     console.log("Hi, I am a middleware");
+//     // res.send("middleware finnished")
+//     next();
+// })
+
 app.use((req, res, next) => {
-    console.log("Hi, I am a middleware");
-    res.send("middleware finnished")
+    req.time = new Date(Date.now()).toString()
+    console.log(req.method, req.time);
     next();
-})
+});
 
 app.get("/", (req, res) => {
-    res.send("server is connected")
-})
+    res.send("server is connected");
+});
 
 app.get("/random", (req, res) => {
     res.send("HI, I am root.")
