76d4fd2e3d064b2b67fccd9476fe51e3cbc433e1
Shashank Soni <shashanksoni815@gmail.com>
2024-11-24 19:24:26 +0530

    middleware

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
+const ExpressError = require("./ExpressError")
 
 // app.use((req, res, next) => {
 //     console.log("Hi, I am a middleware");
 //     // res.send("middleware finnished")
 //     next();
 // })
+
+const classToken = (req, res, next) => {
+    let {token} = req.query;
+    if (token === "giveaccess") {
+        next();
+    }
+    throw new ExpressError(401, "ACCESS DENIED!");
+};
+
+app.get("/api", classToken, (req, res) => {
+    res.send("data");
+});
 
 app.use((req, res, next) => {
     req.time = new Date(Date.now()).toString()
@@ -21,6 +34,14 @@ app.get("/random", (req, res) => {
     res.send("HI, I am root.")
 })
 
+app.get("/err", (req, res, next) => {
+    abcd = abcd
+});
+
+app.use((err, req, res, next) => {
+    console.log("-----ERROR-----")
+    res.send(err);
+})
 
 app.listen(8080, () => {
     console.log("Server is working")
